729035ac8391ea8a9338742092a886b3ffb957c4
tzook.shaked <tzook.shaked@myheritage.com>
2016-05-21 22:32:28 +0300

    Add delete character

---
+++ Assets/Code/Network/character/DeleteCharacter.cs
@@ -0,0 +1,17 @@
+﻿using SimpleJSON;
+using System;
+
+public class DeleteCharacter : HttpProvider
+{
+	private const string DELETE_CHARACTER_URL = "/character/delete";
+
+	public DeleteCharacter(Action<JSONNode> callback) : base(callback){}
+
+	public void Delete(string id)
+	{
+		JSONNode parameters = new JSONClass();
+		parameters["id"] = id;
+
+		performRequest(DELETE_CHARACTER_URL, parameters, true);
+	}
+}
--- Assets/Code/UI/CharacterInfoPageUI.cs
+++ Assets/Code/UI/CharacterInfoPageUI.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using BestHTTP.SocketIO;
 using System;
+using SimpleJSON;
 
 public class CharacterInfoPageUI : MonoBehaviour {
 
@@ -12,6 +13,19 @@ public class CharacterInfoPageUI : MonoBehaviour {
     [SerializeField]
     protected Text txtName;
 
+    [SerializeField]
+    protected MainMenuUI m_mainMenuUI;
+
+    [SerializeField]
+    protected ui_pageMenu m_CharacterSelectionPageMenu;
+
+    protected DeleteCharacter deleteCharacter;
+
+    void Start()
+    {
+        deleteCharacter = new DeleteCharacter(DeleteCharacterResponse);
+    }
+
     public void SetInfo(ActorInfo info)
     {
         actorInstance.UpdateVisual(info);
@@ -19,4 +33,26 @@ public class CharacterInfoPageUI : MonoBehaviour {
         LocalUserInfo.Me.SelectedCharacter = info;
     }
 
+    public void AttemptDeleteCharacter()
+    {
+        SM.LoadingWindow.Register(this);
+        deleteCharacter.Delete(actorInstance.Info.ID);
+    }
+
+    public void DeleteCharacterResponse(JSONNode response)
+    {
+        SM.LoadingWindow.Leave(this);
+
+        if (response["error"] != null)
+        {
+            SM.WarningMessage.ShowMessage(response["error"].ToString());
+        }
+        else
+        {
+            SM.WarningMessage.ShowMessage(response["data"].ToString());
+            LocalUserInfo.Me.SetCharacters(response["data"]);
+            m_CharacterSelectionPageMenu.SwitchTo(0);
+            m_mainMenuUI.LoadPlayerCharacters(LocalUserInfo.Me);
+        }
+    }
 }

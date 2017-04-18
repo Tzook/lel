c63306adf8bb6394af6d6d60654eab09b23a4de0
Tzook <tzook10@gmail.com>
2017-04-18 17:26:05 +0300

    Change server to be prod

--- Assets/Code/Core/Config.cs
+++ Assets/Code/Core/Config.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Config
 {
-    public const string BASE_URL = "http://localhost:5000"; // for testing locally
-    // public const string BASE_URL = "https://lul.herokuapp.com";
+    // public const string BASE_URL = "http://localhost:5000"; // for testing locally
+    public const string BASE_URL = "https://lul.herokuapp.com";
     public const string SOCKET_URL = BASE_URL + "/socket.io/";
 }

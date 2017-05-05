11d804e1cae56923a7e53b83693df86ec6b36331
Tzook <tzook10@gmail.com>
2017-05-05 12:36:33 +0300

    Change host to production

--- Assets/Code/Core/Config.cs
+++ Assets/Code/Core/Config.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Config
 {
-    private const bool LOCAL = !false; // Used for developing locally. This should always be falsy.
+    private const bool LOCAL = false; // Used for developing locally. This should always be falsy.
 
     public const string DOMAIN = LOCAL ? "localhost" : "lul.herokuapp.com";
     public const string BASE_URL = LOCAL ? ("http://" + DOMAIN + ":5000") : ("https://" + DOMAIN);

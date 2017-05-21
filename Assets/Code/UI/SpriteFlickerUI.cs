c73cd376ebe899c27fc81fdf844b1ba00af86cbe
Ben Rokah <ben@marketsroom.com>
2017-05-21 11:56:29 +0300

    Visible Tutorial

---
+++ Assets/Code/UI/SpriteFlickerUI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteFlickerUI : MonoBehaviour
+{
+
+    [SerializeField]
+    Image m_Image;
+
+    [SerializeField]
+    Sprite OriginalSprite;
+
+    [SerializeField]
+    Sprite FlickerSprite;
+
+    [SerializeField]
+    float Delay;
+
+    [SerializeField]
+    float StartDelay;
+
+    void Start()
+    {
+        StartCoroutine(FlickerRoutine());
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(FlickerRoutine());
+    }
+
+    private IEnumerator FlickerRoutine()
+    {
+        yield return new WaitForSeconds(StartDelay);
+
+        while (true)
+        {
+            m_Image.sprite = OriginalSprite;
+
+            yield return new WaitForSeconds(Delay);
+
+            m_Image.sprite = FlickerSprite;
+
+            yield return new WaitForSeconds(Delay);
+        }
+    }
+}

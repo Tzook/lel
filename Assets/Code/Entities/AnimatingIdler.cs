7ed241bade853c8dd4aec79c9165e77d1a02adca
Ben Rokah <benk0914@gmail.com>
2017-09-27 03:28:28 +0300

    Misc Fixes / Level Design (Shell Hill)

---
+++ Assets/Code/Entities/AnimatingIdler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatingIdler : MonoBehaviour {
+
+    [SerializeField]
+    int AnimationIndex;
+
+    private void Start()
+    {
+        GetComponent<Animator>().SetInteger("AnimIndex", AnimationIndex);
+    }
+}

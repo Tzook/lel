2f602b13d454a64e42906fb1e2cf97d68f1b0d80
Ben Rokah <benk0914@gmail.com>
2017-10-29 16:51:59 +0200

    More content

---
+++ Assets/Code/Entities/QuestVisibility.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestVisibility : MonoBehaviour {
+
+    public string HideQuestKey;
+    public QuestState.QuestEnumState HideState;
+
+    private void Start()
+    {
+        switch(HideState)
+        {
+            case QuestState.QuestEnumState.Completed:
+                {
+                    if(LocalUserInfo.Me.ClientCharacter.CompletedQuests.Contains(HideQuestKey))
+                    {
+                        Hide();
+                    }
+
+                    break;
+                }
+            case QuestState.QuestEnumState.InProgress:
+                {
+                    if (LocalUserInfo.Me.ClientCharacter.GetQuest(HideQuestKey) != null)
+                    {
+                        Hide();
+                    }
+
+                    break;
+                }
+            case QuestState.QuestEnumState.NotCompleted:
+                {
+                    if (!LocalUserInfo.Me.ClientCharacter.CompletedQuests.Contains(HideQuestKey))
+                    {
+                        Hide();
+                    }
+
+                    break;
+                }
+            case QuestState.QuestEnumState.NotInProgress:
+                {
+                    if (LocalUserInfo.Me.ClientCharacter.GetQuest(HideQuestKey) == null)
+                    {
+                        Hide();
+                    }
+
+                    break;
+                }
+        }
+    }
+
+    void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
+

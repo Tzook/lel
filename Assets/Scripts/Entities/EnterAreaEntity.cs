e97f7d59fcf0d5f5346f5d17ed3d2a400844a27c
Ben Rokah <benk0914@gmail.com>
2018-02-19 02:38:07 +0200

    WarlockQuest(WIP)

---
+++ Assets/Scripts/Entities/EnterAreaEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnterAreaEntity : MonoBehaviour {
+
+    [SerializeField]
+    UnityEvent OnEnter;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<ActorInstance>() != null)
+        {
+            if (other.GetComponent<ActorInstance>().Info.ID == LocalUserInfo.Me.ClientCharacter.ID)
+            {
+                OnEnter.Invoke();
+            }
+        }
+    }
+}

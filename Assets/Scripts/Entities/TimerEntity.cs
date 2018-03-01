b76afb404038cbd15d0f9e4dc94ac1d62e38b1f5
Ben Rokah <benk0914@gmail.com>
2018-03-01 17:21:02 +0200

    ShroomCOLabs
    TheLabs, sort of

---
+++ Assets/Scripts/Entities/TimerEntity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerEntity : MonoBehaviour {
+
+    [SerializeField]
+    UnityEvent OnReachZero;
+
+    [SerializeField]
+    float CurrentTime;
+
+    bool Activated = true;
+
+    public void SetTime(float Time)
+    {
+        CurrentTime = Time;
+        Activated = false;
+    }
+
+    private void Update()
+    {
+        if(CurrentTime > 0)
+        {
+            CurrentTime -= 1f * Time.deltaTime;
+        }
+        else
+        {
+            if(!Activated)
+            {
+                Activated = true;
+                OnReachZero.Invoke();
+            }
+        }
+    }
+}

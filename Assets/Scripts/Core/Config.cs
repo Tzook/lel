d249d2bfd12aa2c1a67b023fcab7407198ecc4fa
Tzook <Tzook10@gmail.com>
2018-06-07 08:48:23 -0700

    [Config] Make the host dynamic (so webgl build can be hosted in different urls)

--- Assets/Scripts/Core/Config.cs
+++ Assets/Scripts/Core/Config.cs
@@ -4,22 +4,23 @@ using System.Runtime.InteropServices;
 
 public class Config
 {
+    private const string DOMAIN_LOCAL = "localhost";
+    
     #if UNITY_WEBGL
-    private static string hostName;
-    private static bool LOCAL
-    { get 
-    {
-        return (hostName = hostName == null ? GetHostName() : hostName) == DOMAIN_LOCAL;
-    } }
-    [DllImport("__Internal")]
-    private static extern string GetHostName();
+        private static bool LOCAL
+        { get { return DOMAIN == DOMAIN_LOCAL; } }
+
+        [DllImport("__Internal")]
+        private static extern string GetHostName();
+        private static string hostName;
+        public static string DOMAIN
+        { get { return hostName = hostName == null ? GetHostName() : hostName; } }        
     #else 
-    private static bool LOCAL = !true; // Used for developing locally. This should always be falsy.
+        private static bool LOCAL = !true; // Used for developing locally. This should always be falsy.
+        private const string DOMAIN_PROD = "lul.herokuapp.com";
+        public static string DOMAIN = LOCAL ? DOMAIN_LOCAL : DOMAIN_PROD;
     #endif
 
-    public const string DOMAIN_LOCAL = "localhost";
-    public const string DOMAIN_PROD = "lul.herokuapp.com";
-    public static string DOMAIN = LOCAL ? DOMAIN_LOCAL : DOMAIN_PROD;
     public static string BASE_URL = LOCAL ? ("http://" + DOMAIN + ":5000") : ("https://" + DOMAIN);
     public static string SOCKET_URL = BASE_URL + "/socket.io/";
 }

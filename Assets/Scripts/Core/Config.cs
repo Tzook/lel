7dd9f228280f7eaccfee916c9fe282e055752b19
Tzook <tzook10@gmail.com>
2020-03-08 16:17:54 -0700

    Add empty dungeon - turtle cave

--- Assets/Scripts/Core/Config.cs
+++ Assets/Scripts/Core/Config.cs
@@ -5,21 +5,21 @@ using System.Runtime.InteropServices;
 public class Config
 {
     private const string DOMAIN_LOCAL = "localhost";
-    
-    #if UNITY_WEBGL
+
+#if UNITY_WEBGL
         private static bool LOCAL
         { get { return DOMAIN == DOMAIN_LOCAL; } }
 
         [DllImport("__Internal")]
-        private static extern string GetHostName();
+        private static extern string GetHostName(); 
         private static string hostName;
         public static string DOMAIN
         { get { return hostName = hostName == null ? GetHostName() : hostName; } }        
-    #else 
-        private static bool LOCAL = !true; // Used for developing locally. This should always be falsy.
-        private const string DOMAIN_PROD = "lul.herokuapp.com";
-        public static string DOMAIN = LOCAL ? DOMAIN_LOCAL : DOMAIN_PROD;
-    #endif
+#else
+    private static bool LOCAL = true; // Used for developing locally. This should always be falsy.
+    private const string DOMAIN_PROD = "lul.herokuapp.com";
+    public static string DOMAIN = LOCAL ? DOMAIN_LOCAL : DOMAIN_PROD;
+#endif
 
     public static string BASE_URL = LOCAL ? ("http://" + DOMAIN + ":5000") : ("https://" + DOMAIN);
     public static string SOCKET_URL = BASE_URL + "/socket.io/";

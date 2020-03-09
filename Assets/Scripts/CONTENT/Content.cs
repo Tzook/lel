0439babc0c385bddbb332c69157eb0d5af176af2
Tzook <tzook10@gmail.com>
2020-03-09 09:00:56 -0700

    Duplicate boss turtle mob

--- Assets/Scripts/CONTENT/Content.cs
+++ Assets/Scripts/CONTENT/Content.cs
@@ -616,7 +616,7 @@ public class DevSpell: DevSpellBase
 [System.Serializable]
 public class DevMobSpellBase : DevSpellBase
 {
-    [Popup(/* AUTO_GENERATED_MOBS_START */ "NO VALUE", "Bat", "BerriesBush", "BlueBerriesBush", "BlueMushroom", "BossTurtle", "BuffedSquirrel", "BuffedSquirrel_VaultKeeper", "FatRabbit", "FlowerManBandit", "FlowerManBoss", "FrostWizardBoss", "GiantBat", "GreenWorm", "Mole", "OldTurtle", "PirateSailor1", "PirateSailor10", "PirateSailor2", "PirateSailor3", "PirateSailor4", "PirateSailor5", "PirateSailor6", "PirateSailor7", "PirateSailor8", "PirateSailor9", "Plant", "Rabbit", "RabbitBoss", "RedWorm", "Sack", "SmallSquirrel", "Spike", "SpikedTurtle", "Squirrel", "SquirrelBoss", "StoneBlock", "StoneBrick", "StoneGolem", "Thorns", "TomatoesBush", "Turtle", "VilePlant", "Vulture", "VultureRabbit", "Worm", "WormBoss" /* AUTO_GENERATED_MOBS_END */)]
+    [Popup(/* AUTO_GENERATED_MOBS_START */ "NO VALUE", "Bat", "BerriesBush", "BigTurtle", "BlueBerriesBush", "BlueMushroom", "BossTurtle", "BuffedSquirrel", "BuffedSquirrel_VaultKeeper", "FatRabbit", "FlowerManBandit", "FlowerManBoss", "FrostWizardBoss", "GiantBat", "GreenWorm", "Mole", "OldTurtle", "PirateSailor1", "PirateSailor10", "PirateSailor2", "PirateSailor3", "PirateSailor4", "PirateSailor5", "PirateSailor6", "PirateSailor7", "PirateSailor8", "PirateSailor9", "Plant", "Rabbit", "RabbitBoss", "RedWorm", "Sack", "SmallSquirrel", "Spike", "SpikedTurtle", "Squirrel", "SquirrelBoss", "StoneBlock", "StoneBrick", "StoneGolem", "Thorns", "TomatoesBush", "Turtle", "VilePlant", "Vulture", "VultureRabbit", "Worm", "WormBoss" /* AUTO_GENERATED_MOBS_END */)]
     public string[] SpawnMobs;
 }
 
